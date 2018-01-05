ede0326ca8435d4dc698aee2862af5ce62c1cffa
zach <bolanzach@gmail.com>
2018-01-05 08:29:04 -0500

    working on ws

--- app/src/core/network/start.js
+++ app/src/core/network/start.js
@@ -3,16 +3,26 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-
 const app = express();
 const server = http.Server(app);
+const io = socketIO(server);
+
+app.set('port', 5000);
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function(socket) {
+  console.log('connect' + socket);
 });
+
+
+setInterval(function() {
+  io.sockets.emit('message', 'emit socket');
+}, 1000);
+
--- app/src/game.ts
+++ app/src/game.ts
@@ -6,5 +6,9 @@ import {EngineConfig} from "./core/engineConfig";
 
 Engine.Init(new EngineConfig(), function (scene: Scene) {
   
-
+  
+  // let socket = new WebSocket('ws://localhost:5000');
+  // socket.onmessage = function (message) {
+  //   console.log('Connection 1', message.data);
+  // };
 });
